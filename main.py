9d17d16293d315ac313f6ba98b34c65716237f2b
spicycookie-mo <onutumonica@gmail.com>
2023-05-07 22:11:56 +0000

    Changes to be committed: 	modified:   ../../__pycache__/main.cpython-38.pyc 	modified:   ../../main.py

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 # Import what we need from flask
-from flask import Flask
+from flask import Flask, render_template
 
 # Create a Flask app inside `app`
 app = Flask(__name__)
 
+# Define a dictionary of animals and their sounds
+animals = {
+    'cow': 'MOoooOo!',
+    'sheep': 'Baaaah!',
+    'pig': 'Oink oink!'
+}
+
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    # Render the index template and pass in the animals dictionary
+    return render_template('index.html', animals=animals)
 
-@app.route('/cow')
-def cow():
-    return 'MOoooOo you!'
+# Assign a function to be called when the path `/animal/<animal_name>` is requested
+@app.route('/<animal_name>')
+def animal(animal_name):
+    # Look up the sound for the requested animal
+    sound = animals.get(animal_name)
+    # If the animal is not found, return a 404 error
+    if not sound:
+        return 'Animal not found', 404
+    # Render the animal template and pass in the animal name and sound
+    return render_template('animal.html', animal_name=animal_name, sound=sound)
